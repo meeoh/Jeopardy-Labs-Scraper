370d7b7f25aa97308807fbc3c2bf76dfd1c22811
Shameel Abdullah <shameel.abdullah@gmail.com>
2021-01-17 15:37:53 -0500

    add status and final jeopardy

--- javascript/src/App.svelte
+++ javascript/src/App.svelte
@@ -3,16 +3,36 @@
   let output = "";
   let jeopardyUrl = "";
   let doubleJeopardyUrl = "";
+  let finalCategory = "";
+  let finalQuery = "";
+  let finalResponse = "";
+  let name = "";
   async function handleSubmit() {
     if (
       jeopardyUrl.startsWith("https://jeopardylabs.com/play/") &&
       doubleJeopardyUrl.startsWith("https://jeopardylabs.com/play/")
-    )
-      output = JSON.stringify(
-        await Scrape(jeopardyUrl, doubleJeopardyUrl),
-        undefined,
-        2
-      );
+    ) {
+      const finalObject = await Scrape(jeopardyUrl, doubleJeopardyUrl);
+      finalObject.final_jeopardy = {
+        category: finalCategory,
+        questions: [
+          {
+            amount: "FJ",
+            query: finalQuery,
+            response: finalResponse,
+          },
+        ],
+      };
+
+      finalObject.status = {
+        author: `JeopardyLabsScraper - ${name}`,
+        date: new Date().toLocaleDateString(),
+        export_version: "1.00",
+        show_number: 0,
+        unix_timestamp: Date.now(),
+      };
+      output = JSON.stringify(finalObject, undefined, 2);
+    }
   }
 
   function handleCopy() {
@@ -21,10 +41,19 @@
 </script>
 
 <div class="App">
+  <p>Author name:</p>
+  <input bind:value={name} />
   <p>Jeopardy URL:</p>
   <input bind:value={jeopardyUrl} />
   <p>Double Jeopardy URL:</p>
   <input bind:value={doubleJeopardyUrl} />
+  <br />
+  <p>Final Jeopardy Category:</p>
+  <input bind:value={finalCategory} />
+  <p>Final Jeopardy Query:</p>
+  <input bind:value={finalQuery} />
+  <p>Final Jeopardy Response:</p>
+  <input bind:value={finalResponse} />
   <br />
   <br />
   <button on:click={handleSubmit}>Submit</button>
